712dc4e8f5f00cb5f128f965687f59e8406c83c6
jcheron <myaddressmail@gmail.com>
2020-02-17 03:19:39 +0100

    Update initBenchmark.sh

--- .phpbenchmarks/initBenchmark.sh
+++ .phpbenchmarks/initBenchmark.sh
@@ -6,4 +6,4 @@ rm -rf app/cache/*
 chmod -R 777 app/cache
 ./vendor/bin/Ubiquity init-cache
 
-composer install --no-dev --classmap-authoritative --ansi
+composer install --no-dev --optimize-autoloader --classmap-authoritative --ansi
